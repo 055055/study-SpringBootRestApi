f7175e04a708e7e00242af8c2c0d0b9030cb86b9
055055 <snb4301@gmail.com>
2020-12-25 14:50:35 +0900

    스프링 시큐리티 기본 설정

---
+++ src/main/java/com/example/studyrestapi/demo/configs/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.studyrestapi.demo.configs;
+
+import com.example.studyrestapi.demo.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+    //token 저장소로 인메모리 쓰겠다.
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        //AutherizationServer랑 ResourceServer에서 AuthenticationManager를 참조할 수 있도록
+        //Bean으로 노출 시킴. 필요한 경우에만 빈으로 노출
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService)
+            .passwordEncoder(passwordEncoder);
+    }
+
+//    //filter를 거르고 하는 방법
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        //PathRequest.toStaticResources().atCommonLocations() Springboot가 제공하는 Static Resource들에 대한 기본 위치를 다 가져옴
+    }
+
+    //web과 달리 http로 거르는 방법. 아래는 filter를 거쳐야해서 굳이 static에만 접근할거라면 web에서 하는게 낫다.
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .mvcMatchers("/docs/index.html").anonymous()
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
+//
+//    }
+}
